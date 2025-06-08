daeb36ad8f58379a8be81e501edc907107a74551
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-06-08 12:47:23 +0300

    Init devtools, increase time of stale

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./utils/queryClient";
 import TodoList from "./components/TodoList";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TodoList />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
--- src/utils/queryClient.ts
+++ src/utils/queryClient.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1 * 60 * 1000, // Время устаревания данных. Устарвшие данные будут перезапрошены
+      gcTime: 1 * 60 * 1000, // Время удаления данных. inactive -> delete. Освободит память
+    },
+  },
+});
