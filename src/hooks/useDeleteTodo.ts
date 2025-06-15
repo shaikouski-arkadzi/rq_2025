c33b2feee23dc63b61e69333d8a8f1bb8ccc8a92
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-06-15 22:38:48 +0300

    Delete task. Pissimistic update

---
+++ src/hooks/useDeleteTodo.ts
@@ -0,0 +1,42 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { todoListApi } from "../api/requests";
+
+export function useDeleteTodo() {
+  const queryClient = useQueryClient();
+
+  const deleteTodoMutation = useMutation({
+    mutationFn: todoListApi.deleteTodo,
+    onSettled() {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    // data - результат запроса
+    // variables - то что передаем в мутацию. В этом случае id
+    async onSuccess(data, deletedId) {
+      // берем данные в кэше по такому же ключу как мы получали данные
+      const todos = queryClient.getQueryData(
+        todoListApi.getTasksListInfinityQueryOptions().queryKey
+      ); // получаем закэшированные todo
+      if (todos)
+        // вручную удаляем данные из кэша
+        queryClient.setQueryData(
+          todoListApi.getTasksListInfinityQueryOptions().queryKey,
+          (oldData) => {
+            if (!oldData) return oldData;
+            return {
+              ...oldData,
+              pages: oldData.pages.map((page) => ({
+                ...page,
+                data: page.data.filter((item) => item.id !== deletedId),
+              })),
+            };
+          }
+        );
+    },
+  });
+
+  return {
+    handleDelete: deleteTodoMutation.mutate,
+    getIsPending: (id: string) =>
+      deleteTodoMutation.isPending && deleteTodoMutation.variables === id,
+  };
+}
