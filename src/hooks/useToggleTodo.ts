cedbdcde30295362e9f53ed4e5a01c0f3f2d9dba
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-06-16 23:23:32 +0300

    Update task. Optimistic update

---
+++ src/hooks/useToggleTodo.ts
@@ -0,0 +1,62 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { todoListApi } from "../api/requests";
+
+export function useToggleTodo() {
+  const queryClient = useQueryClient();
+
+  const updateTodoMutation = useMutation({
+    mutationFn: todoListApi.updateTodo,
+    // во время вызова мутации
+    onMutate: async (newTodo) => {
+      // Отменяем все исходящие запросы
+      // чтобы они не перезаписали наше оптимистичное обновление
+      await queryClient.cancelQueries({ queryKey: ["todos"] });
+
+      // Получаем предыдущее значение из кэша
+      const previousTodos = queryClient.getQueryData(["todos"]);
+
+      // Дополняем предыдущее значение с новым
+      queryClient.setQueryData(
+        todoListApi.getTasksListInfinityQueryOptions().queryKey,
+        (oldData) => {
+          if (!oldData) return oldData;
+          return {
+            ...oldData,
+            pages: oldData.pages.map((page) => ({
+              ...page,
+              data: page.data.map((todo) =>
+                todo.id === newTodo.id ? { ...todo, ...newTodo } : todo
+              ),
+            })),
+          };
+        }
+      );
+
+      // результат мутации возвращает объект
+      // с измененным вручную значением
+      return { previousTodos };
+    },
+    // Если мутация провалилась используем
+    // контексный объект для отката
+    onError: (err, newTodo, context) => {
+      if (context) {
+        queryClient.setQueryData(["todos"], context.previousTodos);
+      }
+    },
+    // Всегда перезапрашивем после ошибки или успеха
+    async onSettled() {
+      await queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  const toggleTodo = (id: string, done: boolean) => {
+    updateTodoMutation.mutate({
+      id,
+      done: !done,
+    });
+  };
+
+  return {
+    toggleTodo,
+  };
+}
