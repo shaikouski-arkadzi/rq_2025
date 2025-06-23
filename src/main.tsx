46734cbea72dcb4c478f051a861d9fa7528266e8
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-06-23 22:26:19 +0300

    PWA. Offline. Persist storage. In pages version

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { onlineManager } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+
 import "./index.css";
 import App from "./App.tsx";
 import { queryClient } from "./utils/queryClient.ts";
-
 import { store } from "./utils/redux.ts";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+onlineManager.setOnline(navigator.onLine);
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+      // процесс выгрузки из персист хранилища асинхронный
+      // после успешного выполнения выполнится код внутри onSuccess
+      onSuccess={() => {
+        queryClient.resumePausedMutations().then(() => {
+          queryClient.invalidateQueries();
+        });
+      }}
+    >
       <Provider store={store}>
         <App />
       </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>
 );
--- src/utils/queryClient.ts
+++ src/utils/queryClient.ts
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1 * 60 * 1000, // Время устаревания данных. Устарвшие данные будут перезапрошены
-      gcTime: 1 * 60 * 1000, // Время удаления данных. inactive -> delete. Освободит память
+      gcTime: 24 * 60 * 1000, // Время удаления данных. inactive -> delete. Освободит память
     },
   },
 });
